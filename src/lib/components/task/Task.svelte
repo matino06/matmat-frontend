406c2bfaf4de1a8355f1f817fca43e60c18304cb
matino06 <matino0546@gmail.com>
2026-03-11 16:05:06 +0100

    refactor: change mathjax options to not allow menu

--- src/lib/components/task/Task.svelte
+++ src/lib/components/task/Task.svelte
@@ -90,6 +90,10 @@
     if (!task) return;
 
     open = null;
+    window.MathJax = {
+      options: { enableMenu: false },
+    };
+
     const script = document.createElement("script");
     script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js";
     script.async = true;
