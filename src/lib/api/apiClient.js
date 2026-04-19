e6d54e1f7411d58f7b3a03293cc364be0bfa8a67
matino06 <matino0546@gmail.com>
2026-04-19 11:46:02 +0200

    change api url

--- src/lib/api/apiClient.js
+++ src/lib/api/apiClient.js
@@ -1,7 +1,7 @@
 import { auth } from "$lib/config/firebase-config";
 
 export async function apiClient(path, init = {}) {
-  const url = "https://api.matmat.online/api" + path;
+  const url = "https://matmat-backend-production.up.railway.app/" + path;
   const user = auth.currentUser;
   const fetchInit = {
     ...init,
